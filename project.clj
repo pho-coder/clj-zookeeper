de57762a4bcaade53b3f70e488bcb853d953c2c1
Phoenix Zhang <pho.coderzhang@gmail.com>
2016-08-26 11:27:29 +0800

    add client params

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clj-zookeeper "0.2.0-SNAPSHOT"
+(defproject clj-zookeeper "0.3.0-SNAPSHOT"
   :description "A Clojure DSL for Apache ZooKeeper Curator Framework"
   :url "https://github.com/pho-coder/clj-zookeeper"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.zookeeper/zookeeper "3.4.6"]
                  [org.apache.curator/curator-framework "2.9.0"]
                  [org.apache.curator/curator-client "2.9.0" :scope "retry"]]
--- src/clj_zookeeper/zookeeper.clj
+++ src/clj_zookeeper/zookeeper.clj
@@ -32,46 +32,82 @@
     curator-framework))
 
 (defn close
-  []
-  (.close @*curator-framework*))
+  ([]
+   (.close @*curator-framework*))
+  ([^CuratorFrameworkFactory$Builder client]
+   (.close client)))
 
 (defn create
-  [^String path & {:keys [mode data]
-                   :or {mode :persistent}}]
-  (if (nil? data)
-    (.. @*curator-framework*
-        (create)
-        (withMode (zk-create-modes mode))
-        (withACL ZooDefs$Ids/OPEN_ACL_UNSAFE)
-        (forPath path))
-    (.. @*curator-framework*
-        (create)
-        (withMode (zk-create-modes mode))
-        (withACL ZooDefs$Ids/OPEN_ACL_UNSAFE)
-        (forPath path data))))
+  ([^String path & {:keys [mode data]
+                    :or {mode :persistent}}]
+   (if (nil? data)
+     (.. @*curator-framework*
+         (create)
+         (withMode (zk-create-modes mode))
+         (withACL ZooDefs$Ids/OPEN_ACL_UNSAFE)
+         (forPath path))
+     (.. @*curator-framework*
+         (create)
+         (withMode (zk-create-modes mode))
+         (withACL ZooDefs$Ids/OPEN_ACL_UNSAFE)
+         (forPath path data))))
+  ([^CuratorFrameworkFactory$Builder client
+    ^String path & {:keys [mode data]
+                    :or {mode :persistent}}]
+   (if (nil? data)
+     (.. client
+         (create)
+         (withMode (zk-create-modes mode))
+         (withACL ZooDefs$Ids/OPEN_ACL_UNSAFE)
+         (forPath path))
+     (.. client
+         (create)
+         (withMode (zk-create-modes mode))
+         (withACL ZooDefs$Ids/OPEN_ACL_UNSAFE)
+         (forPath path data)))))
 
 (defn delete
-  [^String path]
-  (.. @*curator-framework*
-      (delete)
-      (forPath path)))
+  ([^String path]
+   (.. @*curator-framework*
+       (delete)
+       (forPath path)))
+  ([^CuratorFrameworkFactory$Builder client
+    ^String path]
+   (.. client
+       (delete)
+       (forPath path))))
 
 (defn check-exists?
-  [^String path]
+  ([^String path])
   ((complement nil?)
-   (.. @*curator-framework* (checkExists) (forPath path))))
+   (.. @*curator-framework* (checkExists) (forPath path)))
+  ([^CuratorFrameworkFactory$Builder client
+    ^String path])
+  ((complement nil?)
+   (.. client (checkExists) (forPath path))))
 
 (defn get-children
-  [^String path]
-  (.. @*curator-framework* (getChildren) (forPath path)))
+  ([^String path]
+   (.. @*curator-framework* (getChildren) (forPath path)))
+  ([^CuratorFrameworkFactory$Builder client
+    ^String path]
+   (.. client (getChildren) (forPath path))))
 
 (defn get-data
-  [^String path]
-  (if (check-exists? path)
-    (.. @*curator-framework*
-        (getData)
-        (forPath path))))
+  ([^String path]
+   (if (check-exists? path)
+     (.. @*curator-framework*
+         (getData)
+         (forPath path))))
+  ([^CuratorFrameworkFactory$Builder client
+    ^String path]
+   (if (check-exists? path)
+     (.. client
+         (getData)
+         (forPath path)))))
 
 (defn set-data
-  [^String path ^bytes data]
-  (.. @*curator-framework* (setData) (forPath path data)))
+  ([^String path ^bytes data]
+   (.. @*curator-framework* (setData) (forPath path data)))
+  ([^CuratorFrameworkFactory$Builder client ^String path ^bytes data]
+   (.. client (setData) (forPath path data))))
