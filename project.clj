c46c25481b33db0e4e87e7b7abc6798f24052333
Phoenix Zhang <phoenix.nir@gmail.com>
2017-08-08 18:29:15 +0800

    upgrade dependency

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-zookeeper "0.3.0-SNAPSHOT"
+(defproject clj-zookeeper "0.4.0-SNAPSHOT"
   :description "A Clojure DSL for Apache ZooKeeper Curator Framework"
   :url "https://github.com/pho-coder/clj-zookeeper"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.zookeeper/zookeeper "3.4.6"]
-                 [org.apache.curator/curator-framework "2.9.0"]
-                 [org.apache.curator/curator-client "2.9.0" :scope "retry"]]
-  :profiles {:dev {:dependencies [[org.apache.curator/curator-test "2.9.0" :scope "test"]]}})
+                 [org.apache.zookeeper/zookeeper "3.4.10"]
+                 [org.apache.curator/curator-framework "2.12.0"]
+                 [org.apache.curator/curator-client "2.12.0" :scope "retry"]]
+  :profiles {:dev {:dependencies [[org.apache.curator/curator-test "2.12.0" :scope "test"]]}})
